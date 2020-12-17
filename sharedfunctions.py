b18a5dfbb8e646c5adc63c204772b20e11f88395
Gerry Nelson <45608078+gerrynelson63@users.noreply.github.com>
2020-12-16 20:51:24 -0500

    Update sharedfunctions.py
    added  get_valid_filename

--- sharedfunctions.py
+++ sharedfunctions.py
@@ -670,11 +670,22 @@ def simplejsonresults(resultdata):
         print(json.dumps(resultdata,indent=2))
 
 
+#The get_valid_filename function is taken from https://github.com/django/django/blob/master/django/utils/text.py.
+#The function replaces the characters that are not valid for Linux filsystem in the input string.
+#The comment in the source says:
+#
+#    Return the given string converted to a string that can be used for a clean
+#    filename. Remove leading and trailing spaces; convert other spaces to
+#    underscores; and remove anything that is not an alphanumeric, dash,
+#    underscore, or dot.
 
+
+def get_valid_filename(s):
+	s = str(s).strip().replace(' ', '_')
+	return re.sub(r'(?u)[^-\w.]', '', s)
 
 # getapplicationproperties
 #   20nov2020 initial development
-
 
 def getapplicationproperties():
 
