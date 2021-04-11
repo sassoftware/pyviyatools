197115f5a28958bf71e9e42404c380c990529f04
Gerry Nelson <gerry.nelson@sas.com>
2021-04-11 10:12:22 -0400

    +x

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-
 # Import Python modules
 
 import argparse, sys, configparser, os, inspect
