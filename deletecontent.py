6459146331fa93c6ed364631d9fb8051a9f1ddfa
Gerry Nelson <gerry.nelson@sas.com>
2021-07-12 16:46:03 -0400

    variable name

--- deletecontent.py
+++ deletecontent.py
@@ -124,7 +124,7 @@ if targets[0] is not None:
                reqval=furi+"?recursive=true"
                callrestapi(reqval,reqtype)
 
-        print("NOTE: deleted all content and sub-folders from"+ oath_to_folder)
+        print("NOTE: deleted all content and sub-folders from"+ path_to_folder)
 
         # with content gone recursively delete top folder
         if includefolder:
