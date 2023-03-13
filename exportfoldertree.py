5911b720831a8ce2e384b9921e87de85226c7038
Gerry Nelson <45608078+gerrynelson63@users.noreply.github.com>
2023-03-13 12:31:34 -0400

    add transfer remove

--- exportfoldertree.py
+++ exportfoldertree.py
@@ -12,6 +12,7 @@
 # Change History
 #
 # SEP2022 Added option to specify the root folder to start at
+# MAR2023 Option to remove transfer objects and some general cleanup
 #
 # Copyright Â© 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 #
@@ -49,93 +50,114 @@ parser = argparse.ArgumentParser(description="Export the complete Viya folder tr
 parser.add_argument("-d","--directory", help="Directory to store Export Packages",required='True')
 parser.add_argument("-f","--folder", help="Folder to start export at (root is default)",default="NONE")
 parser.add_argument("-q","--quiet", help="Suppress the are you sure prompt.", action='store_true')
+parser.add_argument("-t","--tranferremove", help="Remove transfer package from SAS Viya after download to JSON file", action='store_true')
 args= parser.parse_args()
 
 basedir=args.directory
 folder=args.folder
 quietmode=args.quiet
+autotranferremove=args.tranferremove
 
 
 # prompt if directory exists because existing json files are deleted
 if os.path.exists(basedir):
 
-	# if the quiet mode flag is not passed then prompt to continue
-	if not quietmode:
+    # if the quiet mode flag is not passed then prompt to continue
+    if not quietmode:
 
-		if version  > 2:
-			areyousure=input("The folder exists any existing json files in it will be deleted. Continue? (Y)")
-		else:
-			areyousure=raw_input("The folder already exists any existing json files in it will be deleted. Continue? (Y)")
-	else:
-		areyousure="Y"
+        if version  > 2:
+            areyousure=input("The folder exists any existing json files in it will be deleted. Continue? (Y)")
+        else:
+            areyousure=raw_input("The folder already exists any existing json files in it will be deleted. Continue? (Y)")
+    else:
+        areyousure="Y"
 
 else: areyousure="Y"
 
 # prompt is Y if user selected Y, its a new directory, or user selected quiet mode
 if areyousure.upper() =='Y':
 
-	path=basedir
+    path=basedir
 
-	# create directory if it doesn't exist
-	if not os.path.exists(path): os.makedirs(path)
-	else:
-		filelist=glob.glob(path+"/*.json")
-		for file in filelist:
-			os.remove(file)
-	
-	reqtype='get'
+    # create directory if it doesn't exist
+    if not os.path.exists(path): os.makedirs(path)
+    else:
+        filelist=glob.glob(path+"/*.json")
+        for file in filelist:
+            os.remove(file)
+    
+    reqtype='get'
 
-	if folder !='NONE':
-		
- 	   folderinfo=getfolderid(folder)
-           results=(folderinfo[3])
-           folderid=results["id"]
-           reqval='/folders/folders/'+folderid+'/members'
+    # folder = NONE means the root folder
+    if folder !='NONE':
+        
+        # get members of folder passed in
+        folderinfo=getfolderid(folder)
+        results=(folderinfo[3])
+        folderid=results["id"]
+        reqval='/folders/folders/'+folderid+'/members'
+    
+    else:
+       # retrieve root folders
+       reqval='/folders/rootFolders'
+    
+    resultdata=callrestapi(reqval,reqtype)
+
+    print(json.dumps(resultdata,indent=2))
+
+    # loop root folders
+    if 'items' in resultdata:
+
+        total_items=resultdata['count']
+        allitems=resultdata['items']
+        
+        if total_items == 0: print("Note: No items returned.")
         else:
-	   # retrieve root folders
-	   reqval='/folders/rootFolders'
-	
-	resultdata=callrestapi(reqval,reqtype)
+            # export each folder and download the package file to the directory
+            i=0
+            for theitem in allitems:
 
-	print(json.dumps(resultdata,indent=2))
+                i=i+1
+                
+                if folder=='NONE':
 
-	# loop root folders
-	if 'items' in resultdata:
+                   # get id for root folders and build URI	
+                   id=theitem["id"]
+                   folderuri='/folders/folders/'+id
+                else:
+                   
+                   #get uri if not root folders
+                   folderuri=theitem["uri"]
 
-		total_items=resultdata['count']
+                # generate a unique name for the package in the Infrastructure Data Server
+                package_name=str(uuid.uuid1())
+                json_name=theitem["name"].replace(" ","")+'_'+str(i)
 
-		returned_items=len(resultdata['items'])
+                # export
+                command=clicommand+' transfer export -u '+folderuri+' --name "'+package_name+'"'
+                print(command)
+                subprocess.call(command, shell=True)
 
-		if total_items == 0: print("Note: No items returned.")
-		else:
-			# export each folder and download the package file to the directory
-			for i in range(0,returned_items):
+                # use the unique name to get the package and download it to a JSON file
+                reqval='/transfer/packages?filter=eq(name,"'+package_name+'")'
+                package_info=callrestapi(reqval,reqtype)
+                package_id=package_info['items'][0]['id']
+                
+                # download exported package
+                completefile=os.path.join(path,json_name+'.json')
+                command=clicommand+' transfer download --file '+completefile+' --id '+package_id
+                print(command)
+                subprocess.call(command, shell=True)
 
-				if folder=='NONE':	
-				   id=resultdata['items'][i]["id"]
-				   folderuri='/folders/folders/'+id
-				else:
-				   folderuri=resultdata['items'][i]["uri"]
+                # if -t set then remove transfer package from infrastructure data server after download
+                if autotranferremove:
+                    print(clicommand+' transfer delete --id '+package_id+"\n")
+                    remTransferObject = subprocess.Popen(clicommand+' transfer delete --id '+package_id, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+                    remTransferObjectOutput = remTransferObject.communicate(b'Y\n')
+                    remTransferObject.wait()
 
-				package_name=str(uuid.uuid1())
-				json_name=resultdata['items'][i]["name"].replace(" ","")+'_'+str(i)
-
-				command=clicommand+' transfer export -u '+folderuri+' --name "'+package_name+'"'
-				print(command)
-				subprocess.call(command, shell=True)
-
-				reqval='/transfer/packages?filter=eq(name,"'+package_name+'")'
-				package_info=callrestapi(reqval,reqtype)
-
-				package_id=package_info['items'][0]['id']
-
-				completefile=os.path.join(path,json_name+'.json')
-				command=clicommand+' transfer download --file '+completefile+' --id '+package_id
-				print(command)
-				subprocess.call(command, shell=True)
-
-	print("NOTE: Viya folders exported to json files in "+path)
+    print("NOTE: Viya folders exported to json files in "+path)
 
 else:
-	 print("NOTE: Operation cancelled")
+     print("NOTE: Operation cancelled")
 
