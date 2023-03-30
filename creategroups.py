224e0c499eb4548d655326641621e0c90665a297
Gerry Nelson <45608078+gerrynelson63@users.noreply.github.com>
2023-03-30 13:52:04 -0400

    updates to creategroups.py (#144)
    * add transfer remove
    
    * fix some indent issues
    
    * check before writing to credentials.json
    
    * update
    
    * credential file write
    
    * fail silently if cannot write to credential file
    
    * bug fix to listcontent.py
    
    * debug
    
    * a few changes to creategroups

--- creategroups.py
+++ creategroups.py
@@ -7,9 +7,9 @@
 # create custom groups and add members using a csv file as input
 # you can also add members to existing groups
 #
-# if the group already exists it will not be added and the http response is printed
-# if the user does not exist it will not be added to the group and the http response is printed
-# if the user is already a member of the group it will not be added to the group and the http response is printed
+# if the group already exists it will not be added
+# if the user does not exist it will not be added to the group
+# if the user is already a member of the group it will not be added to the group
 #
 # none of the above conditions will prevent the processing of additional items in the csv
 #
@@ -17,12 +17,14 @@
 # Change History
 #
 # 03feb2020 Initial development
+# 30mar2023 Added some more error checking
+# 30mar2023 Added -skipfirstrow for situations where first row is a header
 #
 # Format of csv file is two columns
-#  Column 1 group id 
-#  Column 2 group name 
-#  Column 3 is a description
-#  Column 4 member id
+#  Column 1 group id
+#  Column 2 group name
+#  Column 3 a description
+#  Column 4 member id (groupid or userid)
 #
 # For example:
 #group2,"Group 2","My Group 2"
@@ -48,11 +50,15 @@ import csv
 import os
 from sharedfunctions import callrestapi, getfolderid, file_accessible, getidsanduris
 
-# setup command-line arguements    
-parser = argparse.ArgumentParser(description="Create custom groups and establish membership")
+# setup command-line arguements
+parser = argparse.ArgumentParser(description="Create custom groups and establish membership from csv: File Format: column1=groupid,column2=group name,column3=description,optional column4=memberid")
 parser.add_argument("-f","--file", help="Full path to csv file containing groups ",required='True')
+parser.add_argument("--debug", action='store_true', help="Debug")
+parser.add_argument("--skipfirstrow", action='store_true', help="Skip the first row if it is a header")
 args = parser.parse_args()
 file=args.file
+skipfirstrow=args.skipfirstrow
+debug=args.debug
 
 reqtype="post"
 
@@ -65,63 +71,80 @@ allgroups=callrestapi("/identities/groups","get")
 groupslist=[]
 
 if 'items' in allgroups:
-    
+
         total_items=allgroups['count']
         returned_items=len(allgroups['items'])
-             
-        for i in range(0,returned_items): 
-                                
+
+        for i in range(0,returned_items):
+
            groupslist.append(allgroups['items'][i]['id'])
-           
+
 # file can be read
 if check:
 
     with open(file, 'rt') as f:
-        
+
         filecontents = csv.reader(f)
 
+        if skipfirstrow: next(filecontents,None)
+
         for row in filecontents:
-            
-            #print(row)
 
             cols=len(row)
 
-            id=row[0]
-            newgroup=row[1]
-            description=row[2]
+            # skip row and output a message if only 1 column
+            if cols>1:
 
-            data = {}
-            data["id"]=id
-            data['name'] = newgroup
-            data['description'] = description
-            data["state"]="active"
+                # add a default description if only two columns
+                if cols==2: row.append("Add by pyviyatools creategroups.py")
 
-            if id in groupslist:
-                 print("Note: group with name "+newgroup+"  and id "+id+" already exists." )
+                id=row[0]
+                newgroup=row[1]
+                description=row[2]
 
-            else:
-               
-                print ("Note: Trying to creating Group: "+newgroup )
+                data = {}
+                data["id"]=id
+                data['name'] = newgroup
+                data['description'] = description
+                data["state"]="active"
 
-                reqtype='post'
-                reqval="/identities/groups/"
+                # if group does not exist add it
+                if id in groupslist:
+                    print("Note: group with name "+newgroup+"  and id "+id+" already exists." )
+                else:
+                    print ("Note: Trying to creating Group: "+newgroup )
 
-                myresult=callrestapi(reqval,reqtype,data=data,stoponerror=0)
+                    reqtype='post'
+                    reqval="/identities/groups/"
 
-                if myresult != None: print("Note: Group: "+newgroup+" created" )
-                      
-            # 4th column is group membership
-            if cols>=4:
-          
-                member=row[3]
-                print("Note: Trying to add user "+ member+ " to group "+newgroup )
+                    myresult=callrestapi(reqval,reqtype,data=data,stoponerror=0)
 
-                reqval="/identities/groups/"+id+"/userMembers/"+member
-                reqtype='put'
-                myresult=callrestapi(reqval,reqtype,data=data,stoponerror=0)
-                if myresult != None: print("Note: user: "+member+" added to group "+newgroup )
+                    if myresult != None: print("Note: Group: "+newgroup+" created" )
 
+                # 4th column is group membership either a userid or groupid, its optional.
+                if cols>=4 and row[3] !="":
+
+                    member=row[3]
+                    print("Note: Trying to add user "+ member+ " to group "+newgroup )
+
+                    #test that user exists
+                    reqval="/identities/users/"+member
+                    usertest=callrestapi(reqval,'get',noprint=1,stoponerror=0)
+
+                    # user exists try to add to group, if user does not exist print a message
+                    if usertest!=None:
+
+                        reqval="/identities/groups/"+id+"/userMembers/"+member
+                        reqtype='put'
+                        myresult=callrestapi(reqval,reqtype,data=data,noprint=1,stoponerror=0)
+
+                        if myresult != None: print("Note: user: "+member+" added to group "+newgroup )
+                        else: print("Note: user: "+member+" is already a member of group "+newgroup )
+
+                    else: print("WARNING: user: "+member+" does not exist and therefor cannot be added to group.")
+
+            else: print("WARNING: too few columns in row, row must have at least two columns for group id and group.")
 
 else:
-    print("ERROR: cannot read "+file)
-        
+    print("ERROR: cannot read csv file: "+file)
+
--- listcontent.py
+++ listcontent.py
@@ -30,10 +30,10 @@ import argparse, sys
 from sharedfunctions import getfolderid, callrestapi, printresult, getfolderid, getidsanduris, getpath, json
 from datetime import datetime as dt, timedelta as td
 
-# get python version  
+# get python version
 version=int(str(sys.version_info[0]))
 
-# get input parameters	
+# get input parameters
 parser = argparse.ArgumentParser(description="List folder and its sub-folders and content.")
 parser.add_argument("-f","--folderpath", help="Enter the path to the viya folder to start the listing.",required='True')
 parser.add_argument("-v","--verbosecsv", help="Verbose CSV(only used with -o=csv) ", action='store_true' )
@@ -60,51 +60,57 @@ def getfoldercontent(path_to_folder):
 
     # if the folder is found
     if targets[0] is not None:
-        
+
         uri=targets[1]
-        
+
         #get folder content, recursive call returns all children
         reqval=uri+"/members?recursive=true&limit=100000"
         reqtype='get'
 
         if debug: print(reqval)
         folders_result_json=callrestapi(reqval,reqtype)
-        
+
         # add a loop of folder result that adds the parent folder path
         # add the path back into the json for printing
         total_items=folders_result_json['count']
         itemlist=folders_result_json['items']
         returned_items=len(itemlist)
-        
+
         for i in range(0,returned_items):
 
-            contenttype=itemlist[i]["contentType"]
+            if 'contentType' in itemlist[i]: contenttype=itemlist[i]["contentType"]
+            else: contenttype="Unknown"
+
             itemuri=itemlist[i]["uri"]
             name=itemlist[i]["name"]
-                              
-            parentFolderUri=itemlist[i]["parentFolderUri"]   
+
+            parentFolderUri=itemlist[i]["parentFolderUri"]
             path_to_item=getpath(itemuri)
+
+            if path_to_item==None: path_to_item="No folder path."
+            if debug: print(path_to_item,name,contenttype)
 
             itemlist[i]["pathtoitem"]=path_to_item
             itemlist[i]["pathanditemname"]=path_to_item+name
-            #print(path_to_item,name,contenttype) 
 
         newitems = [ ]
 
         # remove folders
         for i in range(0,returned_items):
 
-            if folders_result_json['items'][i]["contentType"]!="folder":
-                newitems.append(folders_result_json['items'][i])
-            
+             if 'contenttype' in folders_result_json['items'][i]:
+
+                if folders_result_json['items'][i]["contentType"]!="folder":
+                    newitems.append(folders_result_json['items'][i])
+
         folders_result_json["count"]=len(newitems)
         folders_result_json["items"]=newitems
-     
+
     return folders_result_json
 
 # root folder, loop sub-folders and print
 if path_to_folder=="/":
-       
+
     # get a list of root folders
 
     # get the json result for each one
@@ -115,28 +121,28 @@ if path_to_folder=="/":
     reqtype='get'
     rootfolderresult=callrestapi(reqval,reqtype)
     rootfolders=rootfolderresult["items"]
-   
+
     z=0
 
     for folder in rootfolders:
-        
+
         foldername="/"+folder["name"]
         result_json=getfoldercontent(foldername)
 
         content_count=result_json['count']
-        
-        # print if there is content in the folder
-        if content_count: 
 
-            # printer header only for the first group    
+        # print if there is content in the folder
+        if content_count:
+
+            # printer header only for the first group
             if z==0: printresult(result_json,output_style,cols)
             else: printresult(result_json,output_style,cols,header=0)
             z=z+1
-            
+
 else:
-    
+
     # print folder content
     result_json=getfoldercontent(path_to_folder)
     printresult(result_json,output_style,cols)
-            
+
 
