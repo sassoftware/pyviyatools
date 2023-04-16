f796016ee0845a7838806f798761f36af254e56f
Tom Starr <88807168+tomstarr@users.noreply.github.com>
2023-04-17 09:02:28 +1200

    applyviyarules and createcaslibjson (#147)

--- importcaslibs.py
+++ importcaslibs.py
@@ -21,6 +21,8 @@
 #
 # Change History
 #
+# 14APR2023 - Added 'superuser' argument
+#
 # Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 #
 #  Licensed under the Apache License, Version 2.0 (the License); you may not use this file except in compliance with the License.
@@ -44,11 +46,13 @@ clicommand=os.path.join(clidir,cliexe)
 
 # get input parameters
 parser = argparse.ArgumentParser(description="Import JSON files that define path-based CASLIBS from directory. All json files in directory will be imported.")
-parser.add_argument("-d","--directory", help="Directory that contains JSON caslib dfinition files to import",required='True')
+parser.add_argument("-d","--directory", help="Directory that contains JSON caslib definition files to import",required='True')
 parser.add_argument("-q","--quiet", help="Suppress the are you sure prompt.", action='store_true')
+parser.add_argument("-su","--superuser", help="Runs the CASLIB create process with superuser permissions.", action='store_true')
 args= parser.parse_args()
 basedir=args.directory
 quietmode=args.quiet
+su=args.superuser
 
 # get python version
 version=int(str(sys.version_info[0]))
@@ -56,59 +60,62 @@ version=int(str(sys.version_info[0]))
 # if the quiet mode flag is not passed then prompt to continue
 if not quietmode:
 
-	if version  > 2:
-		areyousure=input("WARNING: Are you sure? (Y)")
-	else:
-		areyousure=raw_input("WARNING:   Are you sure? (Y)")
+      if version  > 2:
+            areyousure=input("WARNING: Are you sure? (Y)")
+      else:
+            areyousure=raw_input("WARNING:   Are you sure? (Y)")
 else:
-	areyousure="Y"
+      areyousure="Y"
 
 tryimport=0
 
 if areyousure.upper() =='Y':
 
-	# check that directory exists
-	if os.path.isdir(basedir):
+      # check that directory exists
+      if os.path.isdir(basedir):
 
-		# loop files in the directory
-		for filename in os.listdir( basedir ):
+            # loop files in the directory
+            for filename in os.listdir( basedir ):
 
-			fullfile=os.path.join(basedir,filename)
+                  fullfile=os.path.join(basedir,filename)
 
-			# only process json files
-			if filename.lower().endswith('.json'):
+                  # only process json files
+                  if filename.lower().endswith('.json'):
 
-				#create the caslib
-				if '_authorization_' not in filename:
+                        #create the caslib
+                        if '_authorization_' not in filename:
 
-					# get some caslib attributes for the authorization import
-					with open(fullfile) as json_file:
-						data = json.load(json_file)
+                              # get some caslib attributes for the authorization import
+                              with open(fullfile) as json_file:
+                                    data = json.load(json_file)
 
-					caslibname=data['name']
-					casserver=data['server']
+                              caslibname=data['name']
+                              casserver=data['server']
+                              # creates then runs the caslib creation command, with superuser perms where selected
+                              if su:
+                                    command=clicommand+' cas caslibs create path --source-file '+fullfile+' --su'
+                              else:
+                                    command=clicommand+' cas caslibs create path --source-file '+fullfile
+                              print("NOTE: Viya Caslib import attempted from json file "+filename+" in  directory "+basedir  )
+                              print(command)
+                              subprocess.call(command, shell=True)
+                              tryimport=tryimport+1
 
-					command=clicommand+'  cas caslibs create path --source-file '+fullfile
-					print("NOTE: Viya Caslib import attempted from json file "+filename+" in  directory "+basedir  )
-					print(command)
-					subprocess.call(command, shell=True)
-					tryimport=tryimport+1
+                              # apply the authorization if authorization file exists
+                              authfile=os.path.join(basedir,caslibname+'_authorization_.json')
+                              access_file=file_accessible(authfile,'r')
 
-					# apply the authorization if authorization file exists
-					authfile=os.path.join(basedir,caslibname+'_authorization_.json')
-					access_file=file_accessible(authfile,'r')
+                              if access_file==True:
+                                                command=clicommand+' cas caslibs replace-controls --server '+casserver+' --name '+ caslibname+' --force --source-file '+authfile
+                                                print("NOTE: Viya Caslib authorization import attempted from json file "+filename+" in  directory "+basedir  )
+                                                print(command)
+                                                subprocess.call(command, shell=True)           
 
-					if access_file==True:
-						command=clicommand+'  cas caslibs replace-controls --server '+casserver+' --name '+ caslibname+' --force --source-file '+authfile
-						print("NOTE: Viya Caslib authorization import attempted from json file "+filename+" in  directory "+basedir  )
-						print(command)
-						subprocess.call(command, shell=True)
+            if not tryimport: print("NOTE: no caslib files available for import.")
 
-		if not tryimport: print("NOTE: no caslib files available for import.")
-
-	else: print("ERROR: Directory does not exist.")
+      else: print("ERROR: Directory does not exist.")
 else:
-	 print("NOTE: Operation cancelled.")
+       print("NOTE: Operation cancelled.")
 
 
 
