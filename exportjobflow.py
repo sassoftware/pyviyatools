816d274673da15df16af9e8c4bcd0ac629c50096
Gerry Nelson <gerry.nelson@sas.com>
2025-12-10 18:46:54 +0000

    prevent recursion

--- exportjobflow.py
+++ exportjobflow.py
@@ -14,49 +14,54 @@ import argparse, sys, subprocess, uuid, time, os, glob, json, tempfile, re
 
 from sharedfunctions import getidsanduris, callrestapi, getapplicationproperties, printresult, getclicommand, file_accessible
 
-# get python version
-version=int(str(sys.version_info[0]))
+###### FUNCTIONS ######
 
-# get cli location from properties, check that cli is there if not ERROR and stop
-clicommand=getclicommand()
-
-# get input parameters
-parser = argparse.ArgumentParser(description="Export a Viya Job Flow or Flows to packages.")
-
-# Mutually exclusive: exactly one must be provided
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument(
-    "-fn", "--flowname",
-    help="Flow name to export (Viya Job Flow object name). Choose this or pass a flowfile."
-)
-group.add_argument(
-    "-ff", "--flowfile",
-    help="Path to an existing flow definition file to export. Choose this or pass a flowname."
-)
-
-parser.add_argument("-d","--directory", help="Directory to store Export Packages",required='True',default="TEMP")
-parser.add_argument("-t","--transferremove", help="Remove transfer package from SAS Viya after download to JSON file", action='store_true')
-parser.add_argument("--debug", action='store_true', help="Debug: shows the requests file created")
-
-# parse the arguments
-args= parser.parse_args()
-
-flowname=args.flowname
-flowfile=args.flowfile
-directory=args.directory
-debug=args.debug
-autotransferremove=args.transferremove
-
-if flowfile is not None: 
-    check=file_accessible(flowfile,'r')
-    if not check:
-        print("ERROR: Flow definition file "+flowfile+" does not exist or not accessible. Please check the path and try again.")
-        sys.exit()
-
-# create directory if it doesn't exist
-if not os.path.exists(directory) and directory != "TEMP" : os.makedirs(directory)
+def append_unique(items, uri):
+    if uri and uri not in items:
+        items.append(uri)
 
 
+
+def addflowdependencies(flowdetails, data, visited_flows=None):
+    if visited_flows is None:
+        visited_flows = set()
+
+    flow_id = flowdetails.get("id")
+    if flow_id:
+        if flow_id in visited_flows:
+            return
+        visited_flows.add(flow_id)
+
+    jobs = flowdetails.get("jobs", [])
+    for job in jobs:
+        # Detect nested flow URIs and recurse on THEIR details
+        if isinstance(job, str) and "/jobFlowScheduling/flows/" in job:
+            nested_flow = callrestapi(job, "get", acceptType="application/vnd.sas.schedule.flow+json")
+            if nested_flow:
+                addflowdependencies(nested_flow, data, visited_flows)
+            else:
+                print(f"WARNING: Could not load nested flow {job}. Skipping.")
+            continue
+
+        append_unique(data["items"], job)
+
+        jobactresult = callrestapi(job, "get", acceptType="application/vnd.sas.schedule.job+json")
+        if debug:
+            print(json.dumps(jobactresult, indent=4))
+
+        jobrequestURI = jobactresult.get("jobRequestUri")
+        if jobrequestURI:
+            append_unique(data["items"], jobrequestURI)
+
+            jobrequestdetails = callrestapi(
+                jobrequestURI, "get",
+                acceptType="application/vnd.sas.job.execution.job.request+json"
+            )
+            jobDefinitionUri = jobrequestdetails.get("jobDefinitionUri")
+            if jobDefinitionUri:
+                append_unique(data["items"], jobDefinitionUri)
+
+# export a single flow by name
 def exportflow(flowname):
 
     # create a dictionary that will ultimately create the transfer requests file
@@ -93,41 +98,8 @@ def exportflow(flowname):
     data["items"] = []
     data["items"].append(reqval)
 
-    # get job actions from the flow and add them to the requests file
-    # A job action is created when you add a job request to a flow. Job actions are only visible within flows. 
-    #The job action references the job request and includes other information related to priority etc.
-
-    # get jobs in the flow
-    jobs=flowdetails["jobs"]
-   
-    # for each job action in the flow get the job action details
-    for job in jobs:
-        
-        if "/flows/" in job:
-        # if the job is a flow skip for now
-            print("WARNING: Nested flows are not currently supported. Skipping nested flow "+job)
-            continue
-
-        data["items"].append(job)
+    addflowdependencies(flowdetails,data)
     
-        # for each job action get the job request 
-        jobactresult=callrestapi(job,"get",acceptType="application/vnd.sas.schedule.job+json")
-
-        if debug: print(json.dumps(jobactresult, indent=4))
-
-        # get job request URI and add to requests file if present
-        if "jobRequestUri" in jobactresult:
-            jobrequestURI=jobactresult["jobRequestUri"]
-            data["items"].append(jobrequestURI)
-
-            jobrequestdetails=callrestapi(jobrequestURI,"get",acceptType="application/vnd.sas.job.execution.job.request+json")
-            
-            # get job definition URI and add to requests file if present
-            if "jobDefinitionUri" in jobactresult:
-               
-                jobDefinitionUri=jobrequestdetails["jobDefinitionUri"]
-                data["items"].append(jobDefinitionUri)
-
     # create a temp file to hold the requests file that we build
     package_name=flow_actual_name+flowid
     request_file_name=package_name+".json"
@@ -184,6 +156,51 @@ def exportflow(flowname):
         print("NOTE: Viya Job Flow "+flow_actual_name+ " and dependent objects exported to json file "+completefile)
     else:
         print("WARNING: there may be a problem exporting Viya Job Flow "+flow_actual_name+ " to json file "+completefile)
+
+# MAIN CODE ######
+
+# get python version
+version=int(str(sys.version_info[0]))
+# get cli location from properties, check that cli is there if not ERROR and stop
+clicommand=getclicommand()
+
+# get input parameters
+parser = argparse.ArgumentParser(description="Export a Viya Job Flow or Flows to packages.")
+
+# Mutually exclusive: exactly one must be provided
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument(
+    "-fn", "--flowname",
+    help="Flow name to export (Viya Job Flow object name). Choose this or pass a flowfile."
+)
+group.add_argument(
+    "-ff", "--flowfile",
+    help="Path to an existing flow definition file to export. Choose this or pass a flowname."
+)
+
+parser.add_argument("-d","--directory", help="Directory to store Export Packages",required='True',default="TEMP")
+parser.add_argument("-t","--transferremove", help="Remove transfer package from SAS Viya after download to JSON file", action='store_true')
+parser.add_argument("--debug", action='store_true', help="Debug: shows the requests file created")
+
+# parse the arguments
+args= parser.parse_args()
+
+flowname=args.flowname
+flowfile=args.flowfile
+directory=args.directory
+debug=args.debug
+autotransferremove=args.transferremove
+
+if flowfile is not None: 
+    check=file_accessible(flowfile,'r')
+    if not check:
+        print("ERROR: Flow definition file "+flowfile+" does not exist or not accessible. Please check the path and try again.")
+        sys.exit()
+
+# create directory if it doesn't exist
+if not os.path.exists(directory) and directory != "TEMP" : os.makedirs(directory)
+
+
 
 if flowfile is not None:
     # read the flow definition file and get the flow name
